7ad604f3cb6c6ba15f62b4eff7aebb8362e2298b
Bauntyy <t_chesnokov@bk.ru>
2025-04-09 20:47:06 +0400

    Remove wrong comments

--- lab_1/task2/test.py
+++ lab_1/task2/test.py
@@ -4,48 +4,49 @@ from utility_functions import calculate_frequency, read_json, read_file, sort_di
 
 def main():
     """
-    Основная функция программы. Читает зашифрованный текст из файла,
-    вычисляет индекс частот встречаемости символов, дешифрует текст,
-    выводит результаты и сохраняет их в файлах.
+    Main function of the program. Reads the encrypted text from a file,
+    calculates the frequency index of symbols, decrypts the text,
+    displays results, and saves them to files.
 
-    Исключение FileNotFoundError возникает, если файл зашифрованного текста не найден.
-    Исключение UnicodeDecodeError возникает, если возникает проблема с кодировкой файла.
-    Другие исключения обрабатываются общим блоком except.
+    Raises:
+    - FileNotFoundError: If the encrypted text file cannot be found.
+    - UnicodeDecodeError: If there's an issue with the file's encoding.
+    - Exception: Handles all other unforeseen exceptions.
     """
     try:
-        # Чтение зашифрованного текста из файла
+        # Read the encrypted text from the file
         text = read_file(PATH_ENCRYPTED)
-        print("\n", "*" * 40, "Зашифрованный текст", "*" * 40, "\n")
+        print("\n", "*" * 40, "Encrypted Text", "*" * 40, "\n")
         print(text)
 
-        # Вычисление индекса частот символов
+        # Calculate the frequency index of symbols
         frequency_dict = calculate_frequency(text)
-        print("\n", "*" * 40, "Индекс частот", "*" * 40, "\n")
+        print("\n", "*" * 40, "Frequency Index", "*" * 40, "\n")
         sort_dict(frequency_dict)
 
-        # Дешифровка текста
+        # Decrypt the text
         decrypt_key = read_json(PATH_KEY)
-        print("\n", "*" * 40, "Дешифрованный текст", "*" * 40, "\n")
+        print("\n", "*" * 40, "Decrypted Text", "*" * 40, "\n")
         new_text = text
         for original_char, replacement_char in decrypt_key.items():
             new_text = new_text.replace(original_char, replacement_char)
         print(new_text)
 
-        # Вывод ключа дешифрования
+        # Display the decryption key
         crypt_key = read_json(PATH_KEY)
-        print("\n", "*" * 40, "Ключ шифрования", "*" * 40, "\n")
+        print("\n", "*" * 40, "Decryption Key", "*" * 40, "\n")
         print(crypt_key)
 
-        # Запись результатов в файлы
+        # Write the results to files
         write_to_file(PATH_DECRYPTED, new_text)
-        print("\nРезультаты успешно записаны в файлы")
+        print("\nResults have been successfully written to files.")
 
     except FileNotFoundError:
-        print("Ошибка: файл зашифрованного текста не найден")
+        print("Error: The encrypted text file was not found.")
     except UnicodeDecodeError:
-        print("Ошибка: проблема с кодировкой файла")
+        print("Error: There was a problem with the file's encoding.")
     except Exception as e:
-        print(f"Произошла ошибка: {e}")
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
